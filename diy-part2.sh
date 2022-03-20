20b98143f52af3e5c0dede225215394f1dd48810
hh-dxl <66450571+hh-dxl@users.noreply.github.com>
2022-03-20 12:03:24 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,9 +14,9 @@
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 # 替换jerrykuku/luci-theme-argon
 # 修改默认主题把argon换成你要的,三个都要改
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./package/lean/luci-theme-argon-18.06
-sed -i 's/Bootstrap/argon-18.06/g' feeds/luci/collections/luci/Makefile
-sed -i 's/bootstrap/argon-18.06/g' feeds/luci/collections/luci/Makefile
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./package/lean/luci-theme-argon-18.06
+#sed -i 's/Bootstrap/argon-18.06/g' feeds/luci/collections/luci/Makefile
+#sed -i 's/bootstrap/argon-18.06/g' feeds/luci/collections/luci/Makefile
 #sed -i 's/# CONFIG_PACKAGE_luci-theme-argon-18.06 is not set/CONFIG_PACKAGE_luci-theme-argon-18.06=y/g' .config
 # 取消默认主题,自己看着办
-sed -i 's/CONFIG_PACKAGE_luci-theme-bootstrap=y/#CONFIG_PACKAGE_luci-theme-bootstrap is not set/g' .config
+#sed -i 's/CONFIG_PACKAGE_luci-theme-bootstrap=y/#CONFIG_PACKAGE_luci-theme-bootstrap is not set/g' .config
