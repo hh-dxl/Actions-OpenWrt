bc931726d2bd3e6d6b7af97b9c4c1f6a8c6f7d32
hh-dxl <66450571+hh-dxl@users.noreply.github.com>
2023-09-03 00:55:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,11 +15,12 @@ sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # 定制主题
 #git clone https://github.com/rosywrt/luci-theme-rosy.git ./package/lean/luci-theme-rosy
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  ./package/lean/luci-theme-argon2
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  ./package/lean/luci-theme-argon2
+git clone https://github.com/jerrykuku/luci-theme-argon.git ./package/
 # 修改为默认主题
-sed -i 's/Bootstrap/argon2/g' feeds/luci/collections/luci/Makefile
-sed -i 's/bootstrap/argon2/g' feeds/luci/collections/luci/Makefile
-sed -i 's/CONFIG_PACKAGE_luci-theme-bootstrap=y/CONFIG_PACKAGE_luci-theme-argon2=y/g' .config
+#sed -i 's/Bootstrap/argon2/g' feeds/luci/collections/luci/Makefile
+#sed -i 's/bootstrap/argon2/g' feeds/luci/collections/luci/Makefile
+#sed -i 's/CONFIG_PACKAGE_luci-theme-bootstrap=y/CONFIG_PACKAGE_luci-theme-argon2=y/g' .config
 
 # 取消默认主题,自己看着办
 #sed -i 's/CONFIG_PACKAGE_luci-theme-bootstrap=y/#CONFIG_PACKAGE_luci-theme-bootstrap is not set/g' .config
