40011d986a04f9e2a53a82def77e3023f15833c6
hh-dxl <66450571+hh-dxl@users.noreply.github.com>
2023-09-05 00:02:00 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # 定制主题
 #git clone https://github.com/rosywrt/luci-theme-rosy.git ./package/lean/luci-theme-rosy
